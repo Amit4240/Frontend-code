2180f0bbcbe7892a10c28bd2b859b4a307f08327
Amit4240 <nadugeriamit4240@gmail.com>
2025-04-07 12:58:27 +0530

    this  is about arrays in javascript

---
+++ array.js
@@ -0,0 +1,28 @@
+const numberArray = [0,1,2,3];
+
+const mixedArray = [1, 'purva', false];
+
+console.log(numberArray);
+console.log(typeof numberArray);
+console.log(mixedArray);
+console.log(typeof mixedArray);
+
+
+const musicGenres = [ 96, 'Rock', 'Pop', 'Country'];
+console.log(musicGenres);
+
+
+const name = [ 'Purva','Amit'];
+name.shift('Purva');
+
+console.log(name);
+
+
+const sucess = ['hardwork', 'dedication', 'passion', 'patience'];
+console.log(sucess.length);
+console.log(sucess[0]);
+console.log(sucess[1]);
+console.log(sucess[2]);
+console.log(sucess[3]);
+sucess.push('consistency');
+console.log(sucess);
